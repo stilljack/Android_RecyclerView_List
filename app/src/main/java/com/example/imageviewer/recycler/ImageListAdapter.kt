0192496dd1e1d08a23e5d6cf3b2bb1420ab61cb7
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-13 15:30:13 -0700

    complete to build list adapter step 11
    progress continues

--- app/src/main/java/com/example/imageviewer/recycler/ImageListAdapter.kt
+++ app/src/main/java/com/example/imageviewer/recycler/ImageListAdapter.kt
@@ -1,17 +1,28 @@
 package com.example.imageviewer.recycler
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.imageviewer.R
+import com.example.imageviewer.model.ImageData
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.image_item_layout.view.*
 
-class ImageListAdapter (val data: MutableList<ImageListAdapter>) :
+class ImageListAdapter (val data: MutableList<ImageData>) :
     RecyclerView.Adapter<ImageListAdapter.ViewHolder>() {
 
     class ViewHolder(view: View):RecyclerView.ViewHolder(view){
-
+        val image: ImageView = view.thumbnailImage
+        val name: TextView = view.thumbnailName
+        val uri: TextView = view.thumbnailUri
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val viewGroup = LayoutInflater.from(parent.context).inflate(R.layout.image_item_layout, parent, false)
+        return ViewHolder(viewGroup)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
